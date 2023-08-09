3a825c5d5cc0a1b559899a79857f2d99d83342e8
Al Mamun Shisir <almamunshisir770@gmail.com>
2023-08-09 23:36:23 +0600

    n

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -35,7 +35,7 @@ const Banner = () => {
                         </a>
                         <a
                             href="/docs"
-                            className="bg-blue-500 text-white hover:bg-blue-600 py-2 px-4 rounded-full transition duration-300"
+                            className="bg-gray-200 text-black hover:bg-gray-400 py-2 px-4 rounded-full transition duration-300"
                         >
                             Learn Next.js
                         </a>
