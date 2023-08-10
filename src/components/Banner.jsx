22c2493f6eb33ac79abcf4d3dbb0a896cd1a68da
Md Jakaria <jakhossain999@gmail.com>
2023-08-10 12:15:55 +0600

    footer added almost

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,48 +3,48 @@ import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Banner = () => {
-<<<<<<< HEAD
-    const [text]=useTypewriter({
-        words:[
-            "Your one-stop guide to building modern web applications with Next.js.",
-            "Whether you're a beginner or an experienced developer",
-            "we have everything you need to get started and take your projects to the next level."
+    // const [text]=useTypewriter({
+    //     words:[
+    //         "Your one-stop guide to building modern web applications with Next.js.",
+    //         "Whether you're a beginner or an experienced developer",
+    //         "we have everything you need to get started and take your projects to the next level."
 
-        ],
-        loop:true,
-        typeSpeed:30,
-        deleteSpeed:10,
-        delaySpeed:2000,
+    //     ],
+    //     loop:true,
+    //     typeSpeed:30,
+    //     deleteSpeed:10,
+    //     delaySpeed:2000,
 
-    });
-    return (
-        <div className="py-32  h-96 bg-white text-center">
-            <div className="max-w-6xl mx-auto  px-4">
-                <div className="">
-                    <div className=''>
-                        <h2 className="text-5xl font-bold text-gray-800 mb-2">Welcome to NextDocs Hub !</h2>
-                        <p className="text-lg text-gray-600 mt-4">{text}
-                        <Cursor cursorBlinking cursorStyle='' cursorColor='#ffaa17'/> </p>
-                    </div>
-                    <div className=' mt-4 lg:mt-10 '>
-                        <a
-                            href="/docs"
-                            className="bg-black  text-white hover:bg-blue-600 py-2 px-4 rounded-full transition duration-300"
-                        >
-                            Get Started
-                        </a>
-                        <a
-                            href="/docs"
+    // });
+    // return (
+    //     <div className="py-32  h-96 bg-white text-center">
+    //         <div className="max-w-6xl mx-auto  px-4">
+    //             <div className="">
+    //                 <div className=''>
+    //                     <h2 className="text-5xl font-bold text-gray-800 mb-2">Welcome to NextDocs Hub !</h2>
+    //                     <p className="text-lg text-gray-600 mt-4">{text}
+    //                     <Cursor cursorBlinking cursorStyle='' cursorColor='#ffaa17'/> </p>
+    //                 </div>
+    //                 <div className=' mt-4 lg:mt-10 '>
+    //                     <a
+    //                         href="/docs"
+    //                         className="bg-black  text-white hover:bg-blue-600 py-2 px-4 rounded-full transition duration-300"
+    //                     >
+    //                         Get Started
+    //                     </a>
+    //                     <a
+    //                         href="/docs"
 
-                            className="bg-gray-200 text-white hover:bg-black py-2 px-4 rounded-full transition duration-300">
+    //                         className="bg-gray-200 text-white hover:bg-black py-2 px-4 rounded-full transition duration-300">
                             
 
-                            Learn Next.js
-                        </a>
-                    </div>
-                </div>
-            </div>
-=======
+    //                         Learn Next.js
+    //                     </a>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //         </div>)
+            
   const [text] = useTypewriter({
     words: [
       "Your one-stop guide to building modern web applications with Next.js.",
@@ -88,7 +88,6 @@ const Banner = () => {
               Learn Next.js
             </a>
           </div>
->>>>>>> c37fcdf00a6e5ea12258ba4bf54251b6310a915b
         </div>
       </div>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-white text-base-content container mx-auto">
+    <footer className="footer p-10 bg-white text-base-content flex justify-evenly  mx-auto">
       <div>
         <span className="footer-title mx-auto lg:text-2xl lg:mb-10">Welcome </span>
         <Image src={footerLogo} className="w-16 h-16 lg:w-20 lg:h-16 mx-auto"></Image>
@@ -16,35 +16,36 @@ const Footer = () => {
         <span className="footer-title mx-auto lg:text-2xl">Query</span>
         <Link href="/showcase" className="link link-hover mx-auto text-xl font-semibold">Showcase</Link>
         <Link href="/docs" className="link link-hover mx-auto text-xl font-semibold">Docs      </Link>
-        <Link href="/blog" className="link link-hover mx-auto text-xl font-semibold">Blog      </Link>
+        <Link href="/about" className="link link-hover mx-auto text-xl font-semibold">About      </Link>
         <Link href="/analytics" className="link link-hover mx-auto text-xl font-semibold">Analytics </Link>
         <Link href="/templates" className="link link-hover mx-auto text-xl font-semibold">Templates </Link>
         <Link href="/enterprise" className="link link-hover mx-auto text-xl font-semibold">Enterprise</Link>
       </div>
-      <div>
+      <div >
         <span className="footer-title mx-auto lg:text-2xl">Legal</span>
         <a className="link link-hover mx-auto text-xl font-semibold">Terms of use</a>
         <a className="link link-hover mx-auto text-xl font-semibold">Privacy policy</a>
         <a className="link link-hover mx-auto text-xl font-semibold">Cookie policy</a>
         
       </div>
-      <div>
+      <div >
         <span className="footer-title mx-auto lg:text-2xl">Contact Us</span>
         <div className="form-control w-80 ">
           <label className="label mx-auto">
             <span className="label-text lg:text-2xl font-semibold">Enter your Query</span>
           </label>
-          <div className="relative mx-auto">
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-            {/* <input
+          <div className=" mx-auto">
+            {/* <textarea name="Your Feedback" id="" cols="30" rows="10"></textarea> */}
+            <input
               type="text"
-              placeholder="username@site.com"
-              className="input input-bordered w-full pr-16"
-            /> */}
-            <button className="btn btn-ghost absolute top-0 right-0 rounded-l-none">
+            //   placeholder="your feedback"
+              className="input input-bordered w-56 h-44 text-center pr-16"
+            />
+            
+          </div>
+          <button className="btn btn-ghost w-36 mx-auto rounded-xl mt-2 font-semibold text-xl">
               Send
             </button>
-          </div>
         </div>
       </div>
     </footer>
