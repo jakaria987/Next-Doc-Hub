856fb35810a866417d195ae823e3da9e24eba8fb
samantariya <samantaislam355@gmail.com>
2023-09-01 13:33:08 +0600

    framework section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import About from "@/components/About/About";
 import Banner from "@/components/Home/Banner/Banner";
 import ProrammingPage from "./programmingDocs/page";
-import Framework from "@/components/Showcase/FrameWork/Framework";
+import Framework from "@/components/Home/Framework/Framework";
+
 const HomePage = () => {
   return (
     <main>
