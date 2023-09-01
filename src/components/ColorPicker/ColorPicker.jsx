1008fd20902ac40e39215a589460ad74e079fc84
mutanim24 <mutanim24@gmail.com>
2023-09-01 19:51:23 +0600

    update the color picker page

---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react';
+import { SketchPicker } from 'react-color';
+
+const ColorPicker = () => {
+    return (
+        <div>
+            <SketchPicker></SketchPicker>
+            tanim
+           
+        </div>
+    );
+};
+
+export default ColorPicker;
