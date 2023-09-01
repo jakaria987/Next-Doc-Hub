21d209ef8ea933aeda8ccb963027930b386ddc35
samantariya <samantaislam355@gmail.com>
2023-09-01 13:17:20 +0600

    framework section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
-import About from '@/components/About/About';
-import Banner from '@/components/Home/Banner/Banner';
-import Image from 'next/image'
-import ProrammingPage from './programmingDocs/page';
-const HomePage=()=> {
+import About from "@/components/About/About";
+import Banner from "@/components/Home/Banner/Banner";
+import Image from "next/image";
+import ProrammingPage from "./programmingDocs/page";
+import ExtraSection from "@/components/Home/ExtraSection/ExtraSection";
+const HomePage = () => {
   return (
     <main>
       <Banner></Banner>
-      <ProrammingPage/>
-      <About/>
+      <ProrammingPage />
+      <ExtraSection />
+      <About />
     </main>
-     
-  )
-}
+  );
+};
 export default HomePage;
