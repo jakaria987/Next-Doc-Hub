79638707f9aed17d29095acf5821acdd92304c68
Al Mamun <almamunshisir770@gmail.com>
2023-09-04 15:21:27 +0600

    done

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Banner from "@/components/Home/Banner/Banner";
 import ProrammingPage from "./programmingDocs/page";
 import Framework from "@/components/Home/Framework/Framework";
 import JoinCommunity from "@/components/Home/Banner/JoinComunity/JoinComunity";
+import About from "@/components/Home/About/About";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <ProrammingPage />
       <Framework />
       <JoinCommunity/>
+      <About/>
     </main>
   );
 };
